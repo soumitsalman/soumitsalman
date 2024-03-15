372be3d1ca02c3a064eb27816081ae523ebf67a6
soumitsalman <soumitsr@gmail.com>
2024-03-15 17:48:52 -0400

    uploading ubuntu setup file

---
+++ setup.sh
@@ -0,0 +1,64 @@
+# window resizing and copy-paste
+/media/soumitsr/VBox_GAs_7.0.12/VBoxLinuxAdditions.run
+
+sudo apt update 
+
+# python
+sudo apt upgrade python3
+
+# git and pip
+sudo apt install git python3-pip  -y
+
+# git config
+git config --global user.name "soumitsalman"
+git config --global user.email "soumitsr@gmail.com"
+
+# code
+sudo snap install code --classic 
+
+# golang
+sudo snap install go --classic
+
+# postman
+sudo snap install postman
+
+# azure cli
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
+gpg --dearmor | 
+sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
+sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt update
+sudo apt install azure-cli
+
+# docker hub
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+
+# give docker sudo perm	
+sudo usermod -aG docker $USER
+
+# mongosh
+wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
+sudo touch  /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
+
+# brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
+
+
+
+
